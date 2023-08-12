7d1b2ca8cfaa71728f466109ea3fd1e21b3ab720
Stephan Auerhahn <palp@palp.org>
2023-08-12 04:19:14 +0000

    fix test scripts

--- test/pytorch/cu118.sh
+++ test/pytorch/cu118.sh
@@ -10,7 +10,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "validate pytorch version" pip freeze | grep 'pytorch==2.0.1+cu118'
+check "validate pytorch version" pip freeze | grep 'torch==2.0.1+cu118'
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
